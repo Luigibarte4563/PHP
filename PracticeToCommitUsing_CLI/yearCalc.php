c4af6b51d75325ce3ada1d92626998459a6d1e2f
Luigibarte4563 <luigibarte1@gmail.com>
2025-11-24 22:23:15 +0800

    third commit

---
+++ PracticeToCommitUsing_CLI/yearCalc.php
@@ -0,0 +1,10 @@
+<?php
+    $yearOfBorn = 2004;
+    $currentYear = 2025;
+
+    echo "year of born is $yearOfBorn, and current year $currentYear <br>";
+
+    $yourAge = $currentYear - $yearOfBorn;
+
+    echo "You're age is $yourAge";
+?>
