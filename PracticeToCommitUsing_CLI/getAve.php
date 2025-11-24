6c87ea485ae48cada88978472f27c14f051307d8
Luigibarte4563 <luigibarte1@gmail.com>
2025-11-24 22:12:53 +0800

    second commit using CLI

---
+++ PracticeToCommitUsing_CLI/getAve.php
@@ -0,0 +1,15 @@
+<?php
+    
+    $grade1 = 90;
+    $grade2 = 89;
+    $grade3 = 92;
+
+    echo "Getting the  average of these grades <br>";
+    echo "$grade1 + $grade2 + $grade3 <br>";
+
+    $sum = $grade1 + $grade2 + $grade3;
+
+    $average =  $sum / 3;
+
+    echo "The average is $average";
+?>
